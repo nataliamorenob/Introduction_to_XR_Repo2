691ef3769c23608a66a6c80488a4b2d2bb0d0c9e
Natalia Moreno Blasco <nmorenob24@student.oulu.fi>
2025-01-21 11:33:16 +0200

    prueba2windowsantesdeirme21

--- My project/Assets/LightController.cs
+++ My project/Assets/LightController.cs
@@ -1,20 +1,62 @@
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class LightController : MonoBehaviour
 {
     // Declare a public variable to hold a reference to the Light component
     public Light lightComponent;
 
+    // Reference to the XR Controller
+    public XRController rightController;
+
+    // Define the action button
+    private InputDevice rightControllerDevice;
+
     // Start is called before the first frame update
     void Start()
     {
-        // Get the Light component attached to this GameObject
-        lightComponent = GetComponent<Light>();
+        // Find the GameObject by name
+        GameObject pointLight = GameObject.Find("Point Light");
+
+        // If the GameObject is found, get the Light component
+        if (pointLight != null)
+        {
+            lightComponent = pointLight.GetComponent<Light>();
+        }
+        else
+        {
+            Debug.LogError("Point Light GameObject not found!");
+        }
+
+        // Optional: check if the Light component was successfully found
+        if (lightComponent == null)
+        {
+            Debug.LogError("Light component not found on Point Light GameObject!");
+        }
+
+        // Get the XR device for the Right Controller
+        rightControllerDevice = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // You can add logic here to modify the light, if needed
+        // Check if the right controller is available
+        if (rightControllerDevice.isValid)
+        {
+            // Check if the A button is pressed on the Right Controller
+            bool buttonPressed = false;
+            rightControllerDevice.TryGetFeatureValue(CommonUsages.primaryButton, out buttonPressed);
+
+            if (buttonPressed)
+            {
+                // Change light color to red when the A button is pressed
+                if (lightComponent != null)
+                {
+                    lightComponent.color = new Color(1f, 0f, 0f); // Red color
+                }
+            }
+        }
     }
 }
--- My project/Assets/PlayerViewSwitch.cs
+++ My project/Assets/PlayerViewSwitch.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class PlayerViewSwitch : MonoBehaviour
 {
-    // External view point (outside the room)
-    public Transform externalViewPoint;
-    // Internal view point (inside the room --> player starts)
-    public Transform internalViewPoint;
+    // Declare variables to hold references to the view points
+    private Transform externalViewPoint;
+    private Transform internalViewPoint;
 
     // Button for switching (can be configured in the Input Manager)
     public string toggleKey = "space";
 
     private bool isExternal = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find the GameObjects by name in the scene
+        externalViewPoint = GameObject.Find("ExternalViewPoint")?.transform;
+        internalViewPoint = GameObject.Find("InternalViewPoint")?.transform;
+
+        // Check if both view points were found
+        if (externalViewPoint == null)
+        {
+            Debug.LogError("ExternalViewPoint not found in the scene!");
+        }
+        if (internalViewPoint == null)
+        {
+            Debug.LogError("InternalViewPoint not found in the scene!");
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -25,21 +42,28 @@ public class PlayerViewSwitch : MonoBehaviour
     // Switch between the internal and external view points
     void SwitchViewPoint()
     {
-        if (isExternal)
+        // Check if both view points are assigned
+        if (externalViewPoint != null && internalViewPoint != null)
         {
-            // Switch to the internal (room) view point
-            transform.position = internalViewPoint.position;
-            transform.rotation = internalViewPoint.rotation;
+            if (isExternal)
+            {
+                // Switch to the internal (room) view point
+                transform.position = internalViewPoint.position;
+                transform.rotation = internalViewPoint.rotation;
+            }
+            else
+            {
+                // Switch to the external view point
+                transform.position = externalViewPoint.position;
+                transform.rotation = externalViewPoint.rotation;
+            }
+
+            // Toggle the view state
+            isExternal = !isExternal;
         }
         else
         {
-            // Switch to the external view point
-            transform.position = externalViewPoint.position;
-            transform.rotation = externalViewPoint.rotation;
+            Debug.LogError("One or both view points are not assigned!");
         }
-
-        // Toggle the view state
-        isExternal = !isExternal;
     }
 }
-
